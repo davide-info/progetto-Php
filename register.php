728025330cad498e2f4f41b0053e0c51e776d5f7
davide-info <83416046+davide-info@users.noreply.github.com>
2022-03-23 19:45:33 +0100

    inserimento di registrazione form

---
+++ register.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'app/user_repository.php';
+$title = "";
+include_once('templates/header.php');
+include_once("templates/navbar-inc.php");
+
+
+?>
+<div class="container">
+<div class="jumbotron">
+    <h1 class="text-center">
+     Form registrazione
+
+
+    </h1>
+
+</div>
+
+</div>
+
+<?php
+
+?>
